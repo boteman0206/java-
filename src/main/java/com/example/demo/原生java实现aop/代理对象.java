426da0cac5581ea3ae0dd7e169cab1676b1552b0
boteman0206 <1197426145@qq.com>
2019-11-21 21:07:08 +0800

    java的原生接口代理

---
+++ src/main/java/com/example/demo/原生java实现aop/代理对象.java
@@ -0,0 +1,28 @@
+package com.example.demo.原生java实现aop;
+
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class 代理对象 {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException {
+
+        HelloWorldImpl helloWorld = new HelloWorldImpl();
+        // todo jdk 实现的代理只能够代理接口
+        ISayHelloWorld o = (ISayHelloWorld) Proxy.newProxyInstance(helloWorld.getClass().getClassLoader(), (Class<?>[]) helloWorld.getClass().getGenericInterfaces(), new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//                方法会在这里进行拦截
+                System.out.println("权限审核");
+                System.out.println(method);
+                Object invoke = method.invoke(helloWorld, args);// TODO: 这里才是真正进行代码的运行
+                System.out.println("日志记录");
+                return invoke;
+            }
+        });
+
+        System.out.println(o.getClass());
+        o.say(); // todo 这里调用方法会被 invoke方法进行拦截
+    }
+}
